0f984f169ab8cf822dd2a82f407f525a9085957e
mochadwi <iqbal@sampingan.co.id>
2024-04-14 23:53:38 +0700

    Initial Commit

---
+++ Good News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Good News
+//
+//  Created by Mochamad Iqbal Dwi Cahyo on 4/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
